b5fa03c279f6aafe8c99f157d32f639e52531ed9
marijadrljaca <marijaadrljaca@gmail.com>
2023-03-29 11:53:02 +0200

    recenzija za svakog vlasnika

--- src/pages/WalkerListPage/index.jsx
+++ src/pages/WalkerListPage/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Modal, message, Layout } from 'antd';
+import { Modal, message, Layout, Button, Rate, Input } from 'antd';
 import styled from "styled-components";
 import MainMenu from "../../components/MainMenu";
 import { Space, Table, Tag } from 'antd';
@@ -8,6 +8,12 @@ import { Descriptions } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 import { Avatar } from 'antd';
 const { Header, Content, Sider } = Layout;
+const desc = ['užasno', 'loše', 'normalno', 'dobro', 'odlično'];
+const { TextArea } = Input;
+
+const onChange = (e) => {
+  console.log('Change:', e.target.value);
+};
 
 export const WalkerIcon = styled.img `
     heigth: 40px;
@@ -40,7 +46,24 @@ export const Cover = styled.div`
     justify-content: space-around;
 `;
 
-
+export const AddReviewButton = styled.div`
+    width: 300px;
+    height: 2em;
+    background-color: rgba(0,21,41,255);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 0.25em;
+    cursor: pointer;
+    transition: 0.5s;
+    color: aliceblue;
+    font-size: 1.4em;
+    margin-top:640px;
+    margin-left:780px;
+    &:hover {
+        transform: scale(1.15);
+    }
+`;
 
 const WalkerListPage = () => {
   const columns = [
@@ -97,16 +120,28 @@ const WalkerListPage = () => {
     setSelectedWalker(walker);
     setIsModalOpen(true);
   };
+  const showModal1 = (walker) => {
+    setSelectedWalker(walker);
+    setIsModalOpen1(true);
+  };
   const handleOk = () => {
     setIsModalOpen(false);
   };
   const handleCancel = () => {
     setIsModalOpen(false);
   };
+  const handleOk1 = () => {
+    setIsModalOpen1(false);
+  };
+  const handleCancel1 = () => {
+    setIsModalOpen1(false);
+  };
   const [isModalOpen, setIsModalOpen] = useState(false);
-    const [selectedWalker, setSelectedWalker] = useState(null);
+  const [isModalOpen1, setIsModalOpen1] = useState(false);
+  const [selectedWalker, setSelectedWalker] = useState(null);
     
     const [collapsed, setCollapsed] = useState(false);
+    const [value, setValue] = useState(3);
     return (
       <Layout hasSider>
         <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)} style={{
@@ -133,6 +168,7 @@ const WalkerListPage = () => {
              <Modal title="Informacije" open={isModalOpen} onOk={handleOk} onCancel={handleCancel} width={650} 
              okText="Izaberi"
              cancelText="Otkaži"
+             
              >
              <Descriptions title="" size="default" column={2}>
                <Descriptions.Item>
@@ -144,6 +180,28 @@ const WalkerListPage = () => {
                <Descriptions.Item label="Opis">blablababalbalablablablalabal</Descriptions.Item>
                <Descriptions.Item label="Cijena">6KM/h</Descriptions.Item>
              </Descriptions>
+             <Button type="link" onClick={showModal1} >
+                Dodaj recenziju 
+              </Button>
+              <Modal title="Dodaj recenziju" open={isModalOpen1} onOk={handleOk1} onCancel={handleCancel1} okText="Dodaj"
+                    cancelText="Otkaži">
+                     
+                
+                <TextArea
+                  showCount
+                  maxLength={100}
+                  style={{
+                    height: 120,
+                    resize: 'none',
+                  }}
+                  onChange={onChange}
+                  placeholder="Unesite komentar (opciono)"
+                />
+                <span>
+               <Rate tooltips={desc} onChange={setValue} value={value} />
+                 {value ? <span className="ant-rate-text">{desc[value - 1]}</span> : ''}
+             </span>
+              </Modal>
             </Modal>
             </Cover>
           </Page>
