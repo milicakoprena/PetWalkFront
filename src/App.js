1f61a78c16e1e3dd473ff0109923ac3f3804aa1a
tepictanja <tepictanja1146@gmail.com>
2023-04-20 10:33:48 +0200

    ReportProblem i ProblemView

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import ReviewPage from "./pages/ReviewPage";
 import ReportPage from "./pages/ReportPage";
 import EditProfileOwnerPage from "./pages/EditProfileOwnerPage";
 import AccountListPage from "./pages/AccountListPage";
+import ProblemView from "./pages/ProblemView";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
@@ -33,6 +34,7 @@ function App() {
           <Route path="/reviewpage" element={<ReviewPage />} />
           <Route path="/reportpage" element={<ReportPage />} />
           <Route path="/accountlistpage" element={<AccountListPage />} />
+          <Route exact path="/problemview" element={<ProblemView />} />
         </Routes>
       </Router>
       
