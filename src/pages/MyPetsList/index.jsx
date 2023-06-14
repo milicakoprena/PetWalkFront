44140713d2b8d3807fc75103a7b9f967f72ca236
Jovana Majkic <majkicjovana4@gmail.com>
2023-06-14 16:48:56 +0200

    izmjene

--- src/pages/MyPetsList/index.jsx
+++ src/pages/MyPetsList/index.jsx
@@ -1,23 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Modal, Layout, Descriptions } from 'antd';
+import { Modal, Layout, Image } from 'antd';
 import styled from "styled-components";
 import MainMenu from "../../components/MainMenu";
-import { Space, Table } from 'antd';
 import { Button, List } from 'antd';
-import { Avatar } from 'antd';
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 import pozadina from "../resources/pozadina2.jpg"
 import { DeleteOutlined, PictureOutlined } from '@ant-design/icons';
 
 const { Content, Sider } = Layout;
-
-export const StyledTable = styled(Table) `
-    width: 70%;
-    box-shadow: 0 0.15rem 1.75rem 0 rgb(33 40 50 / 35%);
-    background-color: white;
-    border-radius: 10px;
-`;
 
 export const Page = styled.div`
     height: 100vh;
@@ -76,64 +67,6 @@ const MyPetsList = () => {
         setIsModalOpen2(false);
     };
 
-    const columns = [
-        {
-            title: 'Vrsta',
-            dataIndex: 'vrsta',
-            width: '8%',
-        },
-        {
-            title: 'Ime',
-            dataIndex: 'ime',
-            width: '13%',
-        },
-        {
-            title: 'Opis',
-            dataIndex: 'opis',
-            width: '49%',
-        },
-        {
-            title: '',
-            dataIndex: 'show',
-            width: '15%',
-            render: (_, record) => (
-                <Space size="middle" >
-                    <Button type="link" onClick={() => 
-                    {
-                        axios.get(`http://localhost:9000/ljubimci/image/${record.imageName}`, {
-                         headers: {
-                        Authorization: `Bearer ${user.token}`,
-                        responseType: 'arraybuffer',
-                        "Content-Type": 'image/jpeg',
-                    },
-                    })
-                    .then((response) =>
-                    {
-                        setSelectedPetPhoto({
-                            image: `data:image/jpeg;base64,${response.data}`,
-                        })
-                    })
-                    .catch((e) => console.log(e));
-                    showModal2();
-                    }}>Prikaži sliku</Button>
-                </Space>
-            ),
-        },
-        {
-            title: '',
-            dataIndex: 'action',
-            width: '15%',
-            render: (_, record) => (
-                <Space size="middle" >
-                    <Button type="link" onClick={() => 
-                    {
-                        setSelectedPet(record);
-                        showModal();
-                    }}>Obriši</Button>
-                </Space>
-            ),
-        },
-    ];
 
     useEffect(() => {
         axios.get(`http://localhost:9000/vrste`, {
@@ -228,23 +161,25 @@ const MyPetsList = () => {
                                                         <text style={{color: 'black'}}>{item.vrsta}</text>
                                                         <text style={{color: 'black'}}>{item.opis}</text>
                                                     </div>
-                                                    <Button icon={<PictureOutlined /> }  onClick={() => {
-                                                        axios.get(`http://localhost:9000/ljubimci/image/${item.imageName}`, {
-                                                        headers: {
-                                                        Authorization: `Bearer ${user.token}`,
-                                                        responseType: 'arraybuffer',
-                                                        "Content-Type": 'image/jpeg',
-                                                    },
-                                                    })
-                                                    .then((response) =>
-                                                    {
-                                                        setSelectedPetPhoto({
-                                                            image: `data:image/jpeg;base64,${response.data}`,
+                                                    <div>
+                                                        <Button icon={<PictureOutlined /> }  onClick={() => {
+                                                            axios.get(`http://localhost:9000/ljubimci/image/${item.imageName}`, {
+                                                            headers: {
+                                                            Authorization: `Bearer ${user.token}`,
+                                                            responseType: 'arraybuffer',
+                                                            "Content-Type": 'image/jpeg',
+                                                        },
                                                         })
-                                                    })
-                                                    .catch((e) => console.log(e));
-                                                    showModal2();}}></Button>
-                                                    <Button icon={<DeleteOutlined />} style={{marginLeft: '10px'}} onClick={() => {setSelectedPet(item); showModal()}} ></Button>
+                                                        .then((response) =>
+                                                        {
+                                                            setSelectedPetPhoto({
+                                                                image: `data:image/jpeg;base64,${response.data}`,
+                                                            })
+                                                        })
+                                                        .catch((e) => console.log(e));
+                                                        showModal2();}}></Button>
+                                                        <Button icon={<DeleteOutlined />} style={{marginLeft: '10px'}} onClick={() => {setSelectedPet(item); showModal()}} ></Button>
+                                                    </div>
                                                 </div>
                                             }
                                         />
@@ -252,21 +187,20 @@ const MyPetsList = () => {
                                 )}
                                 />
                             </div>
-                            <Modal title="Informacije" open={isModalOpen} onOk={handleOk} onCancel={handleCancel} width={350}
+                            <Modal title="" open={isModalOpen} onOk={handleOk} onCancel={handleCancel} width={350}
                                 okText="Da" cancelText="Ne" style={{ minWidth: '490px' }}
                             >
                                 <p>Da li ste sigurni da želite da obrišete ljubimca?</p>    
                             </Modal>
-                            <Modal title="" open={isModalOpen2} onOk={handleOk2} onCancel={handleCancel2} width={280}
-                                 
+                            <Modal title="" open={isModalOpen2} onOk={handleOk2} onCancel={handleCancel2} width={550}
                                 footer={[]}
                             >
-                                <Descriptions title="" size="default" column={1}>
-                                      <Descriptions.Item>
-                                        <Avatar size={200}  src={selectedPetPhoto.image}
-                                          style={{marginTop: '30px', marginLeft: '10px'}}/>
-                                      </Descriptions.Item>
-                                </Descriptions>  
+                                <Image
+                                    width={500}
+                                    src={selectedPetPhoto.image}
+                                    preview={false}
+                                    style={{marginTop: '17px'}}
+                                /> 
                             </Modal>
                         </Cover>
                     </Page>
